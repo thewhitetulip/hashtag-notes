6f527775b486b97195207c8db9a287f9836ccb61
Suraj Patil <sapatil@live.com>
2015-03-11 21:09:49 +0530

    schema change, removed note title

--- app/src/main/assets/notes_schema-v1.sql
+++ app/src/main/assets/notes_schema-v1.sql
@@ -2,7 +2,6 @@ DROP TABLE IF EXISTS note;
 
 CREATE TABLE note (
     _id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(50) NOT NULL,
     content TEXT NOT NULL,
     created_at BIGINT NOT NULL,
     updated_at BIGINT NOT NULL,
