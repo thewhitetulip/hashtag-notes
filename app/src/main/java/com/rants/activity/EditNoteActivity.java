1e2230eed69b0f268a59cb1cd980888deaa84623
Suraj Patil <sapatil@live.com>
2015-03-17 00:47:41 +0530

    auto complete works now

--- app/src/main/java/com/rants/activity/EditNoteActivity.java
+++ app/src/main/java/com/rants/activity/EditNoteActivity.java
@@ -4,22 +4,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.rants.R;
 import com.rants.data.Note;
+import com.rants.data.dao.NoteDAO;
 import com.rants.util.Strings;
+import com.rants.widget.NotesAdapter;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.inject.Inject;
 
 import roboguice.activity.RoboActionBarActivity;
 import roboguice.inject.ContentView;
@@ -37,7 +49,9 @@ public class EditNoteActivity extends RoboActionBarActivity {
     private static final int EDIT_NOTE_RESULT_CODE = 8;
     private static final String EXTRA_UPDATED_NOTE = "EXTRA_UPDATED_NOTE";
 
-    @InjectView(R.id.note_content) private EditText noteContentText;
+    @InjectView(R.id.note_content) private MultiAutoCompleteTextView noteContentText;
+    @Inject private NoteDAO noteDAO;
+    private ArrayList<NotesAdapter.NoteViewWrapper> notesData;
 
     private Note note;
     public int maxSize=140;
@@ -112,10 +126,44 @@ public class EditNoteActivity extends RoboActionBarActivity {
         btnHash.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.note_content);
-                editText.append("#");
+                MultiAutoCompleteTextView editText = (MultiAutoCompleteTextView) findViewById(R.id.note_content);
+                editText.getText().insert(editText.getSelectionStart(), "#");
             }
         });
+
+        notesData = new ArrayList<>();
+        Set hashtags= new HashSet();
+        for (Note note : noteDAO.fetchAll()) { // Convertir a wrapper
+            String content = note.getContent();
+
+            String c[];
+            if(content.contains("#")){
+                c = content.split("\\s+");
+
+                for(String element:c){
+                    if(element.startsWith("#") && element.length()>1){
+                        hashtags.add(element.replaceAll("[^a-zA-Z#0-9 ]", ""));
+
+                    }
+                }
+            }
+        }
+        String[] tags =  new String[hashtags.size()];
+        Iterator<String> iter = hashtags.iterator();
+
+        int i=0;
+        while(iter.hasNext()){
+            tags[i++]= iter.next();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,tags);
+
+        noteContentText.setRawInputType(InputType.TYPE_CLASS_TEXT
+                |InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
+                |InputType.TYPE_TEXT_FLAG_AUTO_CORRECT
+                | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+
+        noteContentText.setTokenizer(new SpaceTokenizer());
+        noteContentText.setAdapter(adapter);
     }
 
     /** {@inheritDoc} */
@@ -187,5 +235,56 @@ public class EditNoteActivity extends RoboActionBarActivity {
         finish();
     }
 
+    public class SpaceTokenizer implements MultiAutoCompleteTextView.Tokenizer {
+
+        public int findTokenStart(CharSequence text, int cursor) {
+            int i = cursor;
+
+            while (i > 0 && text.charAt(i - 1) != ' ') {
+                i--;
+            }
+            while (i < cursor && text.charAt(i) == ' ') {
+                i++;
+            }
+
+            return i;
+        }
+
+        public int findTokenEnd(CharSequence text, int cursor) {
+            int i = cursor;
+            int len = text.length();
+
+            while (i < len) {
+                if (text.charAt(i) == ' ') {
+                    return i;
+                } else {
+                    i++;
+                }
+            }
+
+            return len;
+        }
+
+        public CharSequence terminateToken(CharSequence text) {
+            int i = text.length();
+
+            while (i > 0 && text.charAt(i - 1) == ' ') {
+                i--;
+            }
+
+            if (i > 0 && text.charAt(i - 1) == ' ') {
+                return text;
+            } else {
+                if (text instanceof Spanned) {
+                    SpannableString sp = new SpannableString(text + " ");
+                    TextUtils.copySpansFrom((Spanned) text, 0, text.length(),
+                            Object.class, sp, 0);
+                    return sp;
+                } else {
+                    return text + " ";
+                }
+            }
+        }
+    }
 
 }
--- app/src/main/java/com/rants/activity/MainActivity.java
+++ app/src/main/java/com/rants/activity/MainActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.view.ActionMode;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
